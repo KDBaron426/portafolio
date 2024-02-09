e602ae0524a6426a5c57c6839fd08172f7c0734f
Karen Baron <kdbaron426@gmail.com>
2024-02-09 02:12:02 -0500

    adding animations for the portafolio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,16 +2,21 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-600">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -21,7 +26,7 @@ const HeroSection = () => {
                 1000,
                 "Web Developer",
                 1000,
-                "Front End Developer",
+                "Frontend Developer",
                 1000,
               ]}
               wrapper="span"
@@ -29,9 +34,9 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lf:text-xl">
-            I&apos;m a Front-end developer passionate about creating interactive and
-            intuitive digital experiences. I am competent in HTML, CSS,
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            I&apos;m a Front-end developer passionate about creating interactive
+            and intuitive digital experiences. I am competent in HTML, CSS,
             JavaScript, ReactJs among others. Let&apos;s create something great
             together, reach out if you&apos;d like to learn more!
           </p>
@@ -40,10 +45,10 @@ const HeroSection = () => {
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Contact Me
+              Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/1ixFUYTdpAQ0f0VkyKd9rOHh4jQYgqkGq/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -51,18 +56,23 @@ const HeroSection = () => {
               </span>
             </Link>
           </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/dani.png"
-              alt="dani-img"
+              alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
