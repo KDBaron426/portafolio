ab8423d37373dd94c0c3beedbbd461d08a3ad179
Karen Baron <kdbaron426@gmail.com>
2024-02-09 01:44:51 -0500

    adding email section and contact

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -107,7 +107,7 @@ const ProjectsSection = () => {
   );
 
   return (
-    <>
+    <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
@@ -140,7 +140,7 @@ const ProjectsSection = () => {
           />
         ))}
       </div>
-    </>
+    </section>
   );
 };
 
